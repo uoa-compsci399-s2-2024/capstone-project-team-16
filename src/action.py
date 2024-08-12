231840ddfdbd20d530989f69df33996c20af18be
Noah <amadtasal10@gmail.com>
2024-08-12 19:12:39 +1200

    Action class initialised
    The barebones of the Action class are complete. Further development needed.

--- src/action.py
+++ src/action.py
@@ -1,1 +1,10 @@
-# Action Class
+import string
+
+
+class Action:
+    def __init__(self, description: string):
+        self.__description = description
+        self.__stats = dict()
+
+    def use(self, item):
+        pass
