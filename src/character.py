6bc9b6d12c8ce8a36d8730c0f0aeef2899e845c5
Finn <flav081@aucklanduni.ac.nz>
2024-08-19 21:09:29 +1200

    added type hinting and getters/setters

--- src/character.py
+++ src/character.py
@@ -1,12 +1,22 @@
 # Character Class
 
 class Character:
-    def __init__(self, name, playable, currentLocation = None, inventory={}):
+    def __init__(self, name: str, playable: bool, currentLocation: str = None, inventory: dict = {}):
         self.name = name #characters name
         self.currentLocation = currentLocation #name of current node
         self.inventory = inventory #dict item:quantity
         self.playable = playable #boolean
 
+    def set_name(self, name):
+        self.name = name
+    def get_name(self) -> str:
+        return self.name
+    
+    def set_location(self, location):
+        self.location = location
+    def get_location(self) -> str:
+        return self.location
+    
     def add_to_inventory(self, item, quantity): #adds an item of specified quantity to inventory 
         if item in self.inventory:
             self.inventory[item] += quantity
@@ -22,12 +32,6 @@ class Character:
         except:
             pass
     
-    def display_inventory(self): #prints inventory
-        print(self.inventory)
-    
-    def update_location(self, location): #updates current node 
-        self.currentLocation = location
-    
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Name: {self.name}\nCurrent Location: {self.currentLocation}\nInventory: {self.inventory}"
     
