0622e8f12bce59ccff1ada4e0d3d7ab1b17a3152
salt <60383410+saltlas@users.noreply.github.com>
2024-08-27 20:25:58 +1200

    added @property getters and setters, added base functions to match other classes

--- src/character.py
+++ src/character.py
@@ -1,11 +1,11 @@
 # Character Class
 
 class Character:
-    def __init__(self, ID: int, name: str, playable: bool, , traits: list[str], currentLocation: str = None, inventory: dict = {}) -> None:
+    def __init__(self, ID: int, name: str, playable: bool, , traits: list[str], current_location: int = None, inventory: dict = {}) -> None:
         self._ID = ID
         self._name = name # character's name
         self._traits = traits # traits the character has
-        self._currentLocation = currentLocation # name of current node
+        self._current_location = current_location # ID of current node
         self._inventory = inventory # dict item:quantity
         self._playable = playable # boolean
 
@@ -23,7 +23,71 @@ class Character:
             else:
                 del self.inventory[item_id]
 
-    
+    # base functions
     def __str__(self) -> str:
         return f"Name: {self.name}\nCurrent Location: {self.currentLocation}\nInventory: {self.inventory}"
+
+    def __repr__(self) -> str:
+        return f"Name: {self.name}\nCurrent Location: {self.currentLocation}\nInventory: {self.inventory}"
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Character):
+            return self.ID == other.ID
+        else:
+            return False
+
+    def __hash__(self) -> int:
+        return hash(self.ID)
+
+    def __lt__(self, other) -> bool:
+        return self.name < other.name
+
+    def __le__(self, other) -> bool:
+        return self.name <= other.name
+
+    def __gt__(self, other) -> bool:
+        return self.name > other.name
+
+    def __ge__(self, other) -> bool:
+        return self.name >= other.name
+
+    # getters and setters
+
+    @property
+    def ID(self) -> int:
+        return self._ID
+
+    @property
+    def name(self) -> str:
+        return self._name
+    @name.setter
+    def name(self, name: str) -> None:
+        self._name = name
     
+    @property
+    def playable(self) -> bool:
+        return self._playable
+    @playable.setter
+    def playable(self, playable: bool) -> None:
+        self._playable = playable
+
+    @property
+    def traits(self) -> list[str]:
+        return self._traits
+    @traits.setter
+    def traits(self, traits: list[str]) -> None:
+        self._traits = traits
+
+    @property
+    def current_location(self) -> int:
+        return self._current_location
+    @current_location.setter
+    def name(self, current_location: int) -> None:
+        self._current_location = current_location
+
+    @property
+    def inventory(self) -> dict:
+        return self._inventory
+    @inventory.setter
+    def inventory(self, inventory: dict) -> None:
+        self._inventory = inventory
