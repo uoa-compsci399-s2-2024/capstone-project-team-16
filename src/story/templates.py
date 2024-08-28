dabc26535f9b92755415613deec94a5b046859a6
Noah <amadtasal10@gmail.com>
2024-08-28 16:13:34 +1200

    Updated tropes and themes quantity
    Tropes and themes are now lists to utilise our multi trope/theme approach.

--- src/story/templates.py
+++ src/story/templates.py
@@ -1,27 +1,25 @@
-def character_template(quantity: int, type_of_char: str, trope: str, theme: str) -> str:
+def character_template(quantity: int, type_of_char: str, tropes: list, themes: list) -> str:
     user_message = ("Create a detailed description of {quantity} {type} character/s including their "
                     "name, a list of traits, age, gender, occupation, a dictionary of "
                     "their inventory and a brief backstory. Keep the character consistent "
-                    "with the the trope of {trope} and the theme of {theme}. Return this "
+                    "with the the tropes of {trope} and the themes of {theme}. Return this "
                     "in JSON format.").format(quantity=quantity, type=type_of_char,
-                                              trope=trope, theme=theme)
+                                              tropes=tropes, themes=themes)
     return user_message
 
 
-def item_template(quantity: int, trope: str, theme: str) -> str:
+def item_template(quantity: int, tropes: list, themes: list) -> str:
     user_message = ("Create a detailed description of {quantity} item/s including its name, "
                     "price, weight and type. Keep the item consistent "
-                    "with the the trope of {trope} and the theme of {theme}. Return this "
-                    "in JSON format.").format(quantity=quantity, trope=trope, theme=theme)
+                    "with the the tropes of {tropes} and the themes of {themes}. Return this "
+                    "in JSON format.").format(quantity=quantity, tropes=tropes, themes=themes)
     return user_message
 
 
-def location_template(location_characters: list, location_items: list, story_trope: str, story_theme: str) -> str:
-    user_message = ("Create a location with a name, these character {characters}, these "
-                    "items {items} and a description of the area. Keep the location "
-                    "consistent with the the trope of {trope} and the theme of {theme}. "
-                    "Return this in JSON format.").format(characters=location_characters, items=location_items,
-                                                          trope=story_trope, theme=story_theme)
+def location_template(quantity: int, tropes: list, themes: list) -> str:
+    user_message = ("Create {quantity} location/s with a name and a description of the area. Keep the location "
+                    "consistent with the the tropes of {tropes} and the themes of {themes}. "
+                    "Return this in JSON format.").format(quantity=quantity, tropes=tropes, themes=themes)
     return user_message
 
 
