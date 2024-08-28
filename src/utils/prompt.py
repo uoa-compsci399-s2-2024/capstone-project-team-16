6c0d9b7f993dfe44ab08a33075e6ca1838637406
salt <60383410+saltlas@users.noreply.github.com>
2024-08-28 17:05:07 +1200

    main and prompt linting

--- src/utils/prompt.py
+++ src/utils/prompt.py
@@ -1,26 +1,34 @@
+""" Utility module for LLM prompt sending """
 from openai import OpenAI
 
-session_messages = list()
+SESSION_MESSAGES = []
 
 
 def chat_with_gpt(client: OpenAI, system_message: str, user_message: str,
                   context: bool, freq_penalty: int = 0, tokens: int = 10,
                   pres_penalty: int = 0, temp: int = 1) -> str:
+    """Sends off a prompt to ChatGPT"""
+
     # when we want to generate a prompt that needs relevant story details
     if context:
         # add the latest prompt to our global list
-        session_messages.append({"role": "user", "content": user_message})
+        SESSION_MESSAGES.append({"role": "user", "content": user_message})
 
         completion = client.chat.completions.create(
             model="gpt-4o-mini-2024-07-18",
-            messages=session_messages,
+            messages=SESSION_MESSAGES,
             frequency_penalty=freq_penalty,
             max_tokens=tokens,
             presence_penalty=pres_penalty,
             temperature=temp
         )
         # add the latest response to our global list
-        session_messages.append({"role": "assistant", "content": completion.choices[0].message.content})
+        SESSION_MESSAGES.append(
+            {
+                "role": "assistant",
+                "content": completion.choices[0].message.content
+            }
+        )
 
     # when we want to generate a prompt that doesn't need story like the creation of weapons
     else:
