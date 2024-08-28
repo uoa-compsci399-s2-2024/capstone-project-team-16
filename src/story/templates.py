b6c4298e296b1021c38d73acde4d238d8f8dde8b
Noah <amadtasal10@gmail.com>
2024-08-28 15:58:30 +1200

    Updated templates
    Now take quantity param to generate 1 or more objects.

--- src/story/templates.py
+++ src/story/templates.py
@@ -1,17 +1,18 @@
-def character_template(type_of_char: str, story_trope: str, story_theme: str) -> str:
-    user_message = ("Create a detailed description of a {type} character including their "
+def character_template(quantity: int, type_of_char: str, trope: str, theme: str) -> str:
+    user_message = ("Create a detailed description of {quantity} {type} character/s including their "
                     "name, a list of traits, age, gender, occupation, a dictionary of "
                     "their inventory and a brief backstory. Keep the character consistent "
                     "with the the trope of {trope} and the theme of {theme}. Return this "
-                    "in JSON format.").format(type=type_of_char, trope=story_trope, theme=story_theme)
+                    "in JSON format.").format(quantity=quantity, type=type_of_char,
+                                              trope=trope, theme=theme)
     return user_message
 
 
-def item_template(story_trope: str, story_theme: str) -> str:
-    user_message = ("Create a detailed description of an item including its name, "
+def item_template(quantity: int, trope: str, theme: str) -> str:
+    user_message = ("Create a detailed description of {quantity} item/s including its name, "
                     "price, weight and type. Keep the item consistent "
                     "with the the trope of {trope} and the theme of {theme}. Return this "
-                    "in JSON format.").format(trope=story_trope, theme=story_theme)
+                    "in JSON format.").format(quantity=quantity, trope=trope, theme=theme)
     return user_message
 
 
