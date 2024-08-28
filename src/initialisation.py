3ceaf51d3da178815cd3cb209c18c950ea416273
salt <60383410+saltlas@users.noreply.github.com>
2024-08-28 23:45:59 +1200

    first draft for pushing purposes

--- src/initialisation.py
+++ src/initialisation.py
@@ -10,7 +10,7 @@ def initialise_game(client):
 	# Choose the theme
 	themes = ["fantasy"]
 
-	# Construct prompt and generate the scene using the tropes and theme
+	# Construct prompt and generate the characters using the tropes and theme
 
 	characters = prompt.chat_with_gpt(client, "", templates.character_template(3, "main", tropes, themes), None, tokens=500)
 
@@ -19,12 +19,17 @@ def initialise_game(client):
 	locations = prompt.chat_with_gpt(client, "", templates.location_template(3, tropes, themes), None, tokens=500)
 
 
-	# Construct prompt and generate characters using the tropes and theme
+	# Construct prompt and generate items using the tropes and theme
 
 	items = prompt.chat_with_gpt(client, "", templates.item_template(3, tropes, themes), None, tokens=500)
 
 
 	current_world = World()
+
+	# mappers go here
+
+	# for character in mapped_characters: current_world.add_character(character)
+	# etc
 
 	print(characters)
 	print(locations)
