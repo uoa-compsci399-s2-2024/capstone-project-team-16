b465033fc0b8184c1db448359b3451269b185ae0
willowtekapaiwaho <wtek097@aucklanduni.ac.nz>
2024-08-29 15:05:18 +1200

    SOUR-8 changed trope ids, made small fixes to trope game functions and added unit tests

--- src/game.py
+++ src/game.py
@@ -22,8 +22,7 @@ def create_tropes(rows):
 def read_csv_file(file_name):
     """Reads a csv file and returns a list of the rows as dictionaries"""
     if not os.path.exists(file_name):
-        print(f"path {file_name} does not exist!")
-        return
+        raise FileNotFoundError(f"path {file_name} does not exist!")
     rows = []
     with open(file_name, encoding="unicode_escape") as csv_file:
         reader = csv.DictReader(csv_file)
@@ -35,34 +34,23 @@ def read_csv_file(file_name):
 def get_random_tropes(tropes_list, num_tropes):
     """Returns a list of with the requested amount of randomly chosen tropes which do not conflict"""
     random_tropes = []
-    conflicts = set([])
 
-    while len(random_tropes) < num_tropes and not len(random_tropes) == len(tropes_list):
+    while len(random_tropes) < num_tropes:
         trope = random.choice(tropes_list)
         if trope not in random_tropes:
             if trope.conflicts is None:
                 random_tropes.append(trope)
-            elif len(conflicts.intersection(set(trope.conflicts))) == 0:
+            elif len(set([r.id_ for r in random_tropes]).intersection(set(trope.conflicts))) == 0:
                 random_tropes.append(trope)
-                conflicts.update(trope.conflicts)
     return random_tropes
 
 
-def select_tropes(plot_tropes_csv_file, protagonist_tropes_csv_file, antagonist_tropes_csv_file):
-    """Selects and returns a list of three plot tropes, a protagonist trope and an antagonist trope from the
+def select_tropes(plot_tropes_csv_file, protagonist_tropes_csv_file, antagonist_tropes_csv_file,
+                  num_plot_tropes, num_prot_tropes, num_ant_tropes):
+    """Selects and returns a list of three plot tropes, one protagonist trope and one antagonist trope from the
     trope csv files"""
-    plot_tropes = create_tropes(read_csv_file(plot_tropes_csv_file))
-    protagonist_tropes = create_tropes(read_csv_file(protagonist_tropes_csv_file))
-    antagonist_tropes = create_tropes(read_csv_file(antagonist_tropes_csv_file))
-
-    chosen_tropes = get_random_tropes(plot_tropes, 3)
-    chosen_tropes.extend(get_random_tropes(protagonist_tropes, 1))
-    chosen_tropes.extend(get_random_tropes(antagonist_tropes, 1))
+    chosen_tropes = get_random_tropes(create_tropes(read_csv_file(plot_tropes_csv_file)), num_plot_tropes)
+    chosen_tropes.extend(get_random_tropes(create_tropes(read_csv_file(protagonist_tropes_csv_file)), num_prot_tropes))
+    chosen_tropes.extend(get_random_tropes(create_tropes(read_csv_file(antagonist_tropes_csv_file)), num_ant_tropes))
 
     return chosen_tropes
-
-
-dirname = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-tropes = select_tropes(os.path.join(dirname, 'src/story/plot_tropes.csv'),
-                       os.path.join(dirname, 'src/story/protagonist_tropes.csv'),
-                       os.path.join(dirname, 'src/story/antagonist_tropes.csv'))
---
+++ tests/trope_tests.py
@@ -0,0 +1,50 @@
+import unittest
+from src.game import *
+
+file_path = os.path.join(os.path.dirname(os.getcwd()), 'src/story/plot_tropes.csv')
+file_path_2 = os.path.join(os.path.dirname(os.getcwd()), 'src/story/protagonist_tropes.csv')
+file_path_3 = os.path.join(os.path.dirname(os.getcwd()), 'src/story/antagonist_tropes.csv')
+
+
+class TropeSelectionTests(unittest.TestCase):
+    def test_read_valid_csv_file(self):
+        result = read_csv_file(file_path)
+        self.assertEqual(type(result), list)
+        for r in result:
+            self.assertEqual(type(r), dict)
+            self.assertEqual(r.keys(), {"id": [], "name": [], "description": [], "conflicts": []}.keys())
+
+    def test_read_invalid_csv_file(self):
+        self.assertRaises(FileNotFoundError, read_csv_file, 'invalid.csv')
+
+    def test_create_trope_objects(self):
+        file_lines = read_csv_file(file_path)
+        result = create_tropes(file_lines)
+        self.assertEqual(type(result), list)
+        self.assertEqual(len(result), len(file_lines))
+        for r in result:
+            self.assertEqual(type(r), Trope)
+
+    def test_get_random_tropes(self):
+        result = get_random_tropes(create_tropes(read_csv_file(file_path)), 3)
+        self.assertEqual(type(result), list)
+        self.assertEqual(len(result), 3)
+        ids = [r.id_ for r in result]
+        for r in result:
+            self.assertEqual(type(r), Trope)
+            if r.conflicts:
+                for c in r.conflicts:
+                    self.assertEqual(ids.__contains__(c), False)
+
+    def test_select_tropes(self):
+        result = select_tropes(file_path, file_path_2, file_path_3, 3, 1, 1)
+        self.assertEqual(type(result), list)
+        self.assertEqual(len(result), 5)
+        ids = [r.id_ for r in result]
+        for r in result:
+            self.assertEqual(type(r), Trope)
+            if r.conflicts:
+                for c in r.conflicts:
+                    self.assertEqual(ids.__contains__(c), False)
+
+
