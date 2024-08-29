d7359834cec2ba4fb3f27412284758621b96a032
willowtekapaiwaho <wtek097@aucklanduni.ac.nz>
2024-08-29 13:24:25 +1200

    SOUR-8 fixed trope selection functions and typos

--- src/game.py
+++ src/game.py
@@ -8,15 +8,15 @@ from src.trope import Trope
 
 def create_tropes(rows):
     """Creates a list of Trope objects from a row"""
-    tropes = []
+    trope_objects = []
     for row in rows:
-        trope = Trope(int(row["id"]), row["title"], row["description"])
+        trope = Trope(int(row["id"]), row["name"], row["description"])
         conflicts = row["conflicts"]
         if conflicts:
             conflicts = conflicts.strip("[]").split(",")
             trope.conflicts = [int(s) for s in conflicts]
-        tropes.append(trope)
-    return tropes
+        trope_objects.append(trope)
+    return trope_objects
 
 
 def read_csv_file(file_name):
@@ -37,13 +37,14 @@ def get_random_tropes(tropes_list, num_tropes):
     random_tropes = []
     conflicts = set([])
 
-    while len(tropes_list) < num_tropes:
+    while len(random_tropes) < num_tropes and not len(random_tropes) == len(tropes_list):
         trope = random.choice(tropes_list)
-        if trope not in tropes_list and not bool(set(trope.conflicts) & conflicts):
-            random_tropes.append(trope)
-            if trope.conflicts:
+        if trope not in random_tropes:
+            if trope.conflicts is None:
+                random_tropes.append(trope)
+            elif len(conflicts.intersection(set(trope.conflicts))) == 0:
+                random_tropes.append(trope)
                 conflicts.update(trope.conflicts)
-
     return random_tropes
 
 
@@ -54,8 +55,14 @@ def select_tropes(plot_tropes_csv_file, protagonist_tropes_csv_file, antagonist_
     protagonist_tropes = create_tropes(read_csv_file(protagonist_tropes_csv_file))
     antagonist_tropes = create_tropes(read_csv_file(antagonist_tropes_csv_file))
 
-    tropes = get_random_tropes(plot_tropes, 3)
-    tropes.extend(get_random_tropes(protagonist_tropes, 1))
-    tropes.extend(get_random_tropes(antagonist_tropes, 1))
+    chosen_tropes = get_random_tropes(plot_tropes, 3)
+    chosen_tropes.extend(get_random_tropes(protagonist_tropes, 1))
+    chosen_tropes.extend(get_random_tropes(antagonist_tropes, 1))
 
-    return tropes
+    return chosen_tropes
+
+
+dirname = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+tropes = select_tropes(os.path.join(dirname, 'src/story/plot_tropes.csv'),
+                       os.path.join(dirname, 'src/story/protagonist_tropes.csv'),
+                       os.path.join(dirname, 'src/story/antagonist_tropes.csv'))
