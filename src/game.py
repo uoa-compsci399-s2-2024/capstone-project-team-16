38c98b87eea92f107d3680639101bc18bc56d686
Finn <flav081@aucklanduni.ac.nz>
2024-09-12 15:31:41 +1200

    updated, did not merge with recent changes

--- src/game.py
+++ src/game.py
@@ -1,5 +1,9 @@
 """Game Loop"""
 
+from actions import move_character
+from utils.templates import demo_choices_movement_template
+from utils.prompt import chat_with_gpt
+
 def choice_selection(choices: list[str]) -> str:
     '''Function to get user input for the choice'''
 
@@ -20,9 +24,6 @@ def get_choices() -> list[str]:
     ''''Function to get choices when that is written'''
     pass
 
-def get_location(world: World) -> Location:
-    '''Function to get next location'''
-    pass
 
 def display_location(location: Location) -> None:
     '''Function to display location'''
@@ -34,21 +35,23 @@ def pass_choice(choice: str) -> None:
 
 
 
-
-
-
-
-
-
-
-
-def game_loop(player: Character, world: World) -> None:
+def game_loop(player: Character, world: World, client: OpenAI) -> None:
 
     game_over = False
 
     while not game_over:
-        current_location = get_location(world)
-        display_location(current_location)
-        user_choice = choice_selection(world.choices)
-        pass_choice(user_choice)
-        
+        current_location = player.current_location
+        choices = chat_with_gpt(
+            client=client,
+            system_message="You are a knowledgable chatbot that generates choices",
+            user_message=demo_choices_movement_template(current_location.neighbors),
+            context=False
+        ) #need a mapper to convert the output to a list of choices
+        player_choice = choice_selection(choices)
+        move_character(
+            character_object=player,
+            current_location=current_location,
+            new_location=player_choice,
+            client=client,
+            world_object=world
+        )
