5de1c2927cb5fb6c5e25b004ef45dd344606beba
Noah <amadtasal10@gmail.com>
2024-09-19 21:06:09 +1200

    Paths updated and playthrough folder

---
+++ src/playthroughs/README.md
@@ -0,0 +1,2 @@
+# This is a dummy file so the playthroughs folder can be commited
+# The folder is empty now but gets populated as games are completed
--- src/utils/playthroughs.py
+++ src/utils/playthroughs.py
@@ -1,13 +1,13 @@
 import os
 import shutil
 
-temp_file_path = str(os.getcwd()) + '\\story\\temp_story_store.txt'
+temp_file_path = str(os.getcwd()) + '/src/story/temp_story_store.txt'
 
 
 def create_temp_story_file():
     if not os.path.exists(temp_file_path):
-        with open(temp_file_path, 'x'):
-            pass
+        with open(temp_file_path, 'x') as file:
+            file.close()
 
 
 def write_scene_and_choice(scene_str: str, choice_str: str) -> None:
@@ -21,14 +21,14 @@ def write_scene_and_choice(scene_str: str, choice_str: str) -> None:
 
 def save_playthrough_as_file() -> None:
     playthrough_name = str(input("Enter the name of your playthrough: "))
-    playthrough_file_path = str(os.getcwd()) + f'\\playthroughs\\{playthrough_name}.txt'
+    playthrough_file_path = str(os.getcwd()) + f'/src/playthroughs/{playthrough_name}.txt'
     
-    with open(playthrough_file_path, 'x'):
-        pass
+    with open(playthrough_file_path, 'x') as file:
+        file.close()
     
     shutil.copy(temp_file_path, playthrough_file_path)
 
 
 def wipe_temp_file() -> None:
     with open(temp_file_path, 'w') as file:
-        pass
+        file.close()
