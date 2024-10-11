39dd5afe389f221e9b2d6822cddf4d5a2d1f6d73
finnlaville-moore <finnlavillemoore@outlook.com>
2024-10-12 10:19:31 +1300

    Added a setup file to create an exe

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+
+'''Run this from cmd, will build an excutable file. Note that filepaths 
+    need to be changed for all data files. Will also need pyinstaller installed'''
+
+def build_executable():
+    script_name = 'src/main.py'
+
+
+    command = [
+        'py',
+        "-m",
+        "PyInstaller",
+        "--onefile",
+        '--add-data=src/story/antagonist_tropes.csv;.',
+        '--add-data=src/story/plot_tropes.csv;.',
+        '--add-data=src/story/protagonist_tropes.csv;.',
+        '--add-data=src/story/temp_story_store.txt;.',
+        '--add-data=src/story/themes.txt;.',
+        script_name
+    ]
+
+    try:
+        subprocess.run(command, check=True)
+        print("Build completed")
+    except subprocess.CalledProcessError as e:
+        print("Error", e)
+
+if __name__ == "__main__":
+    build_executable()
