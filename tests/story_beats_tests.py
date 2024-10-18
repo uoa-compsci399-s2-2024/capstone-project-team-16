398fb0e4e454478436f5028bd24c2c00c7d272a9
willowtekapaiwaho <wtek097@aucklanduni.ac.nz>
2024-10-18 23:22:50 +1300

    SOUR-46 fixed story beats tests after changes made

--- tests/story_beats_tests.py
+++ tests/story_beats_tests.py
@@ -8,16 +8,16 @@ class TestStoryBeats(unittest.TestCase):
         random.seed(200)
         result = change_beat(0)
         self.assertEqual(type(result), tuple)
-        self.assertEqual(type(result[0]), tuple)
-        self.assertEqual(result[0], list(BEATS.items())[1])
+        self.assertEqual(type(result[0]), int)
+        self.assertEqual(result[0], 1)
         self.assertEqual(result[1], False)
 
     def test_change_beat_end(self):
         random.seed(200)
         result = change_beat(8)
         self.assertEqual(type(result), tuple)
-        self.assertEqual(type(result[0]), tuple)
-        self.assertEqual(result[0], list(BEATS.items())[9])
+        self.assertEqual(type(result[0]), int)
+        self.assertEqual(result[0], 9)
         self.assertEqual(result[1], True)
 
     def test_change_beat_no_change(self):
@@ -26,8 +26,3 @@ class TestStoryBeats(unittest.TestCase):
         self.assertEqual(type(result), tuple)
         self.assertEqual(result[0], None)
         self.assertEqual(result[1], False)
-
-    def test_get_first_beat(self):
-        result = get_initial_beat()
-        self.assertEqual(type(result), tuple)
-        self.assertEqual(result, list(BEATS.items())[0])
