55ae451d79df53a8ff761d5b434af0a63d0c4c79
Noah <amadtasal10@gmail.com>
2024-10-24 23:03:49 +1300

    updated intro

--- src/initialisation.py
+++ src/initialisation.py
@@ -56,7 +56,11 @@ def print_description() -> None:
     print("In this game, you will embark on a thrilling quest")
     print("filled with danger, mystery, and magic. Your choices")
     print("will determine the outcome of your adventure.")
-    print("NOTE: If you should wish to view the data of this playthrough then enter INFO.")
+    print()
+    print("If you should wish to view the data of this playthrough then enter info.")
+    print("If you should wish to view the map of this playthrough then enter info.")
+    print("If you should wish to save this playthrough then enter save.")
+    print("Thank you for playing and enjoy Adventure's Call!")
     print()
 
 
